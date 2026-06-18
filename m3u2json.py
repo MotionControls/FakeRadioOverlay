57603b8cbfba9b406f601c653dc7bd388f6b8def
MotionControls <bprogers43@tntech.edu>
2026-06-18 01:36:39 -0500

    Overhauled playlist conversion.

--- m3u2json.py
+++ m3u2json.py
@@ -1,33 +1,40 @@
 import os, json
 import music_tag as mt
 from pathlib import Path as pl
+import ffmpeg
 
 musicDir = "music/"
 m3uDir = "m3u/"
 stationDir = "stations/"
 
+musicLen = 0
+compList = []
+compFile = open(stationDir + "comp.txt", "w")
+
 fileList = os.listdir(m3uDir)
 fileList = [f for f in fileList if os.path.isfile(m3uDir + f)]
 for file in fileList:
-    jsonArr = []
+    musicList = []
     playlist = open(m3uDir + file)
+    station = open(stationDir + os.path.splitext(file)[0] + ".txt", "w")
     for line in playlist:
-        line = line.rstrip('\n')
-        #print(os.path.splitext(line)[1])
-        if(os.path.splitext(line)[1] == ".flac"): continue
-        song = mt.load_file((musicDir + os.path.basename(line)))
-        jsonArr.append(json.dumps({
-            "path": os.path.basename(line),
-            "name": str(song["title"]),
-            "artist": str(song["artist"]).split(";")[0],
-            "album": str(song["album"])
-        }))
-    station = open(stationDir + os.path.splitext(file)[0] + ".json", "w")
-    station.write("[")
-    for i in range(len(jsonArr)):
-        station.write(jsonArr[i])
-        if(i < len(jsonArr) - 1):
-            station.write(",")
-    station.write("]")
+        line = os.path.basename(line.rstrip('\n'))
+        basename, extension = os.path.splitext(line)
+        if(extension != ".mp3"):
+            print("Converting " + line + ".")
+            stream = ffmpeg.input(musicDir + line)
+            stream = ffmpeg.output(stream, musicDir + basename + ".mp3", loglevel="error", )
+            stream = ffmpeg.overwrite_output(stream)
+            ffmpeg.run(stream)
+        
+        index = musicLen
+        if(basename not in compList):
+            compList.append(basename)
+            compFile.write(str(musicLen) + "//" + basename + ".mp3" + "\n")
+            musicLen += 1
+        else:
+            index = compList.index(basename)
+        station.write(str(index) + ",")
+    station.write(";")
     station.close()
     playlist.close()
